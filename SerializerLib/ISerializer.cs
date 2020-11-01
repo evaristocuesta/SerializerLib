6ac8b36e4dd6bee90642207723712212ade0ba9d
evaristocuesta <evaristocg@gmail.com>
2020-11-01 17:55:42 +0100

    Comments added

--- SerializerLib/ISerializer.cs
+++ SerializerLib/ISerializer.cs
@@ -2,12 +2,33 @@
 
 namespace SerializerLib
 {
+    /// <summary>
+    /// Interface to serialize and deserialize
+    /// </summary>
     public interface ISerializer
     {
-        T Deserialize<T>(string xml);
+        /// <summary>
+        /// Deserialize a string
+        /// </summary>
+        /// <typeparam name="T">Type of the deserialized returned object</typeparam>
+        /// <param name="stringToDeserialize">String to be deserialized</param>
+        /// <returns>Type T deserialized object</returns>
+        T Deserialize<T>(string stringToDeserialize);
 
+        /// <summary>
+        /// Deserialize a file
+        /// </summary>
+        /// <typeparam name="T">Type of the deserialized returned object</typeparam>
+        /// <param name="file">File's path to be deserialized</param>
+        /// <returns>Type T deserialized object</returns>
         T DeserializeFile<T>(string file);
 
+        /// <summary>
+        /// Serialize an object
+        /// </summary>
+        /// <typeparam name="T">Type of the object to be serilized</typeparam>
+        /// <param name="obj">Object to be serialized</param>
+        /// <returns>String serialized object</returns>
         string Serialize<T>(T obj);
     }
 }
--- SerializerLib/XMLSerializer.cs
+++ SerializerLib/XMLSerializer.cs
@@ -3,20 +3,41 @@ using System.Xml.Serialization;
 
 namespace SerializerLib
 {
+    /// <summary>
+    /// Class to seriliaze to xml and deserialize from xml 
+    /// </summary>
     public class XMLSerializer : ISerializer
     {
-        public T Deserialize<T>(string xml)
+        /// <summary>
+        /// Deserialize a xml string
+        /// </summary>
+        /// <typeparam name="T">Type of the deserialized returned object</typeparam>
+        /// <param name="stringToDeserialize">String to be deserialized</param>
+        /// <returns>Type T deserialized object</returns>
+        public T Deserialize<T>(string stringToDeserialize)
         {
             XmlSerializer serializer = new XmlSerializer(typeof(T));
-            StringReader stringReader = new StringReader(xml);
+            StringReader stringReader = new StringReader(stringToDeserialize);
             return (T)serializer.Deserialize(stringReader);
         }
 
+        /// <summary>
+        /// Deserialize a file
+        /// </summary>
+        /// <typeparam name="T">Type of the deserialized returned object</typeparam>
+        /// <param name="file">File's path to be deserialized</param>
+        /// <returns>Type T deserialized object</returns>
         public T DeserializeFile<T>(string file)
         {
             return Deserialize<T>(File.ReadAllText(file));
         }
 
+        /// <summary>
+        /// Serialize an object
+        /// </summary>
+        /// <typeparam name="T">Type of the object to be serilized</typeparam>
+        /// <param name="obj">Object to be serialized</param>
+        /// <returns>String serialized object</returns>
         public string Serialize<T>(T obj)
         {
             XmlSerializer serializer = new XmlSerializer(typeof(T));
