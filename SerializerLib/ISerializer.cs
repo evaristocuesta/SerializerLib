b444d7eeb7a454cf8ad41e21bc70acfd7ab5ffcf
evaristocuesta <evaristocg@gmail.com>
2020-11-01 16:35:12 +0100

    Add project files.

---
+++ SerializerLib/ISerializer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SerializerLib
+{
+    public interface ISerializer
+    {
+        T Deserialize<T>(string xml);
+
+        T DeserializeFile<T>(string file);
+
+        string Serialize<T>(T obj);
+    }
+}
---
+++ SerializerLib/XMLSerializer.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+
+namespace SerializerLib
+{
+    public class XMLSerializer : ISerializer
+    {
+        public T Deserialize<T>(string xml)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+            StringReader stringReader = new StringReader(xml);
+            return (T)serializer.Deserialize(stringReader);
+        }
+
+        public T DeserializeFile<T>(string file)
+        {
+            return Deserialize<T>(File.ReadAllText(file));
+        }
+
+        public string Serialize<T>(T obj)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+            string xml = "";
+            using (Stream stream = new MemoryStream())
+            {
+                serializer.Serialize(stream, obj);
+                stream.Position = 0;
+                var sr = new StreamReader(stream);
+                xml = sr.ReadToEnd();
+            }
+            return xml;
+        }
+    }
+}
